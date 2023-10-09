f3cb8d196ca1a69338ab35332a32fe20a794c2e0
Yaang Vu <yaangvu@gmail.com>
2023-10-09 17:57:39 +0700

    Add functions: removeRequestParams mergeRequestParams requestToArray

--- src/Base/Utility/HasRequest.php
+++ src/Base/Utility/HasRequest.php
@@ -2,25 +2,54 @@
 
 namespace YaangVu\LaravelBase\Base\Utility;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 
 trait HasRequest
 {
     /**
-     * @param object $request
-     * @param        ...$params
+     * @param object   $request
+     * @param string[] ...$params
      *
-     * @return Request|object|mixed
+     * @return object
      */
-    public function removeParams(object $request, ...$params): mixed
+    public function removeRequestParams(object $request, string|array $params): object
     {
-        if ($request instanceof Request)
-            $request->except($params);
-        else {
-            foreach ($params as $param)
-                unset($request->{$param});
-        }
+        if (is_string($params))
+            $params = [$params];
+
+        $isHttpRequest = $request instanceof Request;
+
+        foreach ($params as $param)
+            if ($isHttpRequest) $request->request->remove($param);
+            else unset($request->{$param});
 
         return $request;
+    }
+
+    /**
+     * @param object               $request
+     * @param array<string, mixed> $params
+     *
+     * @return object
+     */
+    public function mergeRequestParams(object $request, array $params): object
+    {
+        if ($request instanceof Request)
+            $request->merge($params);
+        else
+            $request = (object)array_merge((array)$request, $params);
+
+        return $request;
+    }
+
+    /**
+     * @param object $request
+     *
+     * @return array
+     */
+    public function requestToArray(object $request): array
+    {
+        return ($request instanceof Request || $request instanceof Model) ? $request->toArray() : (array)$request;
     }
 }
